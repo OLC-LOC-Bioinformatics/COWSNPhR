faa30ffaf2d388a559a22ac7c047990e7d59361a
adamkoziol <adam.koziol@inspection.gc.ca>
2019-01-15 16:11:38 -0500

    Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+__author__ = 'stuber, adamkoziol'
+
+setup(
+    name="vSNP",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[os.path.join('vsnp', 'vSNP.py')
+             ],
+    license='MIT',
+    author='Adam Koziol',
+    author_email='adam.koziol@canada.ca',
+    description='USDA APHIS Veterinary Services (VS) Mycobacterium tuberculosis complex, '
+                'mainly M. bovis, and Brucella sp. SNP pipeline. Genotyping from whole genome sequence (WGS) '
+                'outputting BAM, VCF, SNP tables and phylogentic trees.',
+    url='https://github.com/USDA-VS/vSNP',
+    long_description=open('README.md').read(),
+    install_requires=['olctools',
+                      'biopython',
+                      'pandas',
+                      'pytest',
+                      'xlsxwriter',
+                      'validator_helper']
+)
