cc2ab98525dc8dcd5bf4c8f0d6457ca9bdcae2a7
Adam Koziol <adam.koziol@canada.ca>
2023-06-21 10:11:51 -0400

    Modify version in setup to use version.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""
+Set up the package
+"""
+
+__author__ = 'adamkoziol'
+
+# Standard inputs
+from distutils.util import convert_path
+import os
+
+# Third party inputs
 from setuptools import setup, find_packages
-__author__ = 'stuber, adamkoziol'
+
+# Find the version
+version = {}
+with open(convert_path(os.path.join('cowsnphr_src', 'version.py')), 'r') as version_file:
+    exec(version_file.read(), version)
+
 
 setup(
-    name="cowsnphr",
-    version="0.0.30",
+    name='cowsnphr',
+    version=version['__version__'],
     packages=find_packages(),
     include_package_data=True,
     entry_points={
@@ -15,7 +31,7 @@ setup(
     },
     license='MIT',
     author='Adam Koziol',
-    author_email='adam.koziol@canada.ca',
+    author_email='adam.koziol@inspection.gc.ca',
     description='Single Nucleotide Variant Calling Pipeline',
     url='https://github.com/OLC-LOC-Bioinformatics/COWSNPhR',
     long_description=open('README.md').read(),
